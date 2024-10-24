07ddb6d4c56fa3f1363e5dc2bbb4b78487aa7a5f
ROGER ZHANG <jaigue134@gmail.com>
2024-10-24 15:23:22 +0800

    Colcon #1

--- install/bringup/share/bringup/launch/robot_state_publisher.launch.py
+++ install/bringup/share/bringup/launch/robot_state_publisher.launch.py
@@ -10,32 +10,24 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
 
-    pkg_share = FindPackageShare(package = 'robot_description').find('robot_description')
-    default_model_path = os.path.join(
-        pkg_share, 
-        'urdf/robot.urdf'
-    )
+    pkg_name = 'robot_description'
+    pkg_share = FindPackageShare(package = pkg_name).find(pkg_name)
+    default_model_path = os.path.join(pkg_share, 'urdf/robot.urdf')
 
     model = LaunchConfiguration('model')
-    use_sim_time_arg = LaunchConfiguration('use_sim_time')
 
-    model_launch_arg = DeclareLaunchArgument(
-        name='model', 
-        default_value=default_model_path,
-        description='Absolute path to robot urdf file'
+    model_arg = DeclareLaunchArgument(
+            name='model', 
+            default_value=default_model_path,
+            description='Absolute path to robot urdf file'
     )
-
-    use_sim_time_arg = DeclareLaunchArgument(
-            'use_sim_time',
-            default_value='false',
-            description='Use simulation (Gazebo) clock if true'
-    )
-
+    
     robot_state_publisher_node = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
         parameters=[{'robot_description': Command(['xacro ', model])}]
     )
+    
     joint_state_publisher_node = Node(
         package='joint_state_publisher',
         executable='joint_state_publisher',
@@ -43,8 +35,7 @@ def generate_launch_description():
     )
 
     return LaunchDescription([
-        model_launch_arg,
-        use_sim_time_arg,
+        model_arg,
         robot_state_publisher_node,
         joint_state_publisher_node
     ])
